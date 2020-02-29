93470d4076627ae470cdc614b77e69d885394934
D Ther <dther@opslab.space>
2020-03-01 02:16:13 +0630

    fmt

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "security_group" {
-    value = aws_security_group.security_group
+  value = aws_security_group.security_group
 }
--- variables.tf
+++ variables.tf
@@ -1,25 +1,25 @@
 variable "name" {
-    default = null
-    type = string
+  default = null
+  type    = string
 }
 
 variable "description" {
-    default = null
-    type = string
+  default = null
+  type    = string
 }
 
 variable "vpc_id" {
-    type = string
+  type = string
 }
 
 variable "ingress_rules" {
-    default = []
-    description = "A list of custom ingress rules to apply"
-    type = any
+  default     = []
+  description = "A list of custom ingress rules to apply"
+  type        = any
 }
 
 variable "egress_rules" {
-    default = []
-    description = "A list of custom egress rules to apply"
-    type = any
+  default     = []
+  description = "A list of custom egress rules to apply"
+  type        = any
 }
